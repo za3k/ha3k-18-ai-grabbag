670e4a47112a640aa465997bae651db2680abeb6
Zachary Vance <za3k@za3k.com>
2023-11-18 17:48:16 -0500

    Support files in whisper.sh

--- whisper.sh
+++ whisper.sh
@@ -1,5 +1,11 @@
-cd $(dirname -- "$0")/whisperX
-. ./venv/bin/activate
-whisperx --model large-v2 --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --compute_type int8 --language en --output_format tsv - >/dev/null 2>/dev/null
-cat ./-.tsv
-rm ./-.tsv
+if [ "$#" -ge 1 ]; then
+    ffmpeg -i "$1" -ar 16000 -ac 1 -c:a pcm_s16le -f wav - 2>/dev/null
+else
+    cat
+fi | (
+    cd $(dirname -- "$0")/whisperX
+    . ./venv/bin/activate
+    whisperx --model large-v2 --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --compute_type int8 --language en --output_format tsv - >/dev/null 2>/dev/null
+    tail -n+2 ./-.tsv
+    rm ./-.tsv
+)
