ddd1796b1e1ea03167c8df7ce8e2a175089956ae
Zachary Vance <za3k@za3k.com>
2023-11-18 19:31:58 -0500

    Add LLM support

--- README.md
+++ README.md
@@ -1,40 +1,44 @@
 This was my attempt to catch up on the 2022-2023 AI tools which have come out recently, based on "transformers".
 
-[ ] Transcribe D&D audio as a test
-[ ] TranscribeAudio() function for library
+If you install CUDA, all of the below will run significantly faster.
 
-[ ] Say() function for library
-[ ] Chat() function for library using HuggingFace
-[ ] Chat(continue=conversation) function for library using HuggingFace
 [ ] Draw() function for library using StableDiffusion (return PIL object)
-[ ] Listen() function for library
+[ ] Translate() function for translation
 
-My goal is to write a python library, so that I can write command like (or the equivalent with async/await):
+My goal is to write a python library, so that I can write commands like (or the equivalent with async/await):
 
-    # Speech transcription, using whisper/whisperx
-    annotated_text = TranscribeAudio("audio.mp3")
+```
+# Speech transcription, using whisper/whisperx
+annotated_text = TranscribeAudio("audio.mp3")
 
-    # Talking to LLMs, which respond to text with text. Uses HuggingFace
-    reply, conversation = Chat("What is a healthy, tasty meal to eat for dinner?", engine="LLaMA-2") # GPT-3 not supported
+# Talking to LLMs, which respond to text with text. Uses HuggingFace
+reply, conversation = Chat("What is a healthy, tasty meal to eat for dinner?", engine="LLaMA-2") # GPT-3 not supported
 
-    # Speech synthesis
-    Say(reply)
-    Say(reply, as="Jerry Seinfeld")
+# Speech synthesis
+Say(reply)
+Say(reply, as="Jerry Seinfeld")
 
-    # Human language translation, using LLMs again
-    auf_deutsch = Translate(reply, "German")
-    print(auf_deutch)
-    Say(auf_deutsch)
+# Human language translation, using LLMs again
+auf_deutsch = Translate(reply, "German")
+print(auf_deutch)
+Say(auf_deutsch)
 
-    # Creating pictures, using stable diffusion
-    picture = Draw("a sarcastic cat, in a simple cartoon style")
-    picture.show()
-    picture.save("cat.jpg")
-    
-    # Wait for a spoken command
-    annotated_prompt = Listen()
+# Creating pictures, using stable diffusion
+picture = Draw("a sarcastic cat, in a simple cartoon style")
+picture.show()
+picture.save("cat.jpg")
 
-## Install whisperx
+# Wait for a spoken command (not done)
+annotated_prompt = Listen()
+```
+
+## Install espeak-ng (speech synthesis)
+
+```
+pacman -S espeak-ng
+```
+
+## Install whisperx (speech recognition)
 
 ```
 yay -S python310
@@ -44,7 +48,18 @@ yay -S python310
 git clone https://github.com/m-bain/whisperX
 cd whisperX
 virtualenv -p python3.10 venv
-. ./venv/bin/activate
+. venv/bin/activate
 pip install -r requirements.txt
 pip install -e .
+```
+
+## Install llama.cpp
+
+```
+git clone https://github.com/ggerganov/llama.cpp
+cd llama.cpp
+python -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+wget --directory-prefix models -c https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf
 ```
--- ai.py
+++ ai.py
@@ -1,6 +1,8 @@
 import asyncio
 import collections
+import os
 import subprocess
+import sys
 import tempfile
 
 import typing
@@ -11,6 +13,8 @@ Language = str
 ConversationToken = str
 AnnotatedText = collections.namedtuple("AnnotatedText", ["start", "end", "text"])
 Image = Any # Will correct later
+
+DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 def tmpfile(b):
     # Can't just use io.BytesIO because subprocess wants a fileno
@@ -24,18 +28,30 @@ async def TranscribeAudio(file: Union[str, IO[AnyStr]]) -> list[AnnotatedText]:
         with open(file, "r") as f:
             return await TranscribeAudio(f)
 
-    text = subprocess.check_output("~/ha3k-18-ai-grabbag/whisper.sh", shell=True, stdin=file, stderr=subprocess.DEVNULL).decode('utf8').strip()
+    text = subprocess.check_output(os.path.join(DIR, "whisper.sh"), stdin=file, stderr=subprocess.DEVNULL).decode('utf8').strip()
 
     ret: list[AnnotatedText] = []
-    for line in text.split("\n")[1:]:
+    for line in text.split("\n"):
         start, end, text = line.split("\t")
         ret.append(AnnotatedText(int(start), int(end), text))
     return ret
 
-async def Chat(prompt: str, conversation: Optional[ConversationToken], engine: ChatEngine="LLaMA-2") -> Tuple[str, ConversationToken]:
-    pass
-    #return reply, conversation
-    return "", ""
+async def Chat(prompt: str, conversation: Optional[ConversationToken]=None, reply_length=1000, engine: ChatEngine="LLaMA-2") -> Tuple[str, ConversationToken]:
+    llama_dir = os.path.join(DIR, "llama.cpp")
+    LLAMA_PROMPT = """[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n{prompt}[/INST] """
+    LLAMA_PROMPT = """[INST]{prompt}[/INST]\n\n"""
+    prompt = (conversation or "") + LLAMA_PROMPT.format(prompt=prompt)
+
+    command = ["./main",
+        "-m", "models/llama-2-7b-chat.Q4_K_M.gguf",
+        "--verbose-prompt",
+        "-n", str(reply_length),
+        "-p", prompt,
+        "-ngl", "4000",
+    ]
+    conversation = subprocess.check_output(command, stderr=subprocess.DEVNULL, cwd=llama_dir).decode("utf8").strip()
+    reply = conversation.removeprefix(prompt).strip()
+    return reply, conversation
 
 async def Say(text: str, language=None) -> None:
     subprocess.check_output("speak", input=text.encode("utf8"), stderr=subprocess.DEVNULL)
@@ -69,6 +85,18 @@ async def main():
         re_transcribed = await TranscribeAudio(test_audio)
         print("  ", re_transcribed[0].text)
         print("Speech recognition test end.")
+    if False:
+        print("LLM chat test start.")
+        reply, convo = await Chat("Explain the single most important difference between London and Moscow.")
+        print(reply)
+        reply, convo = await Chat("Okay, say one more cool thing about each city.", convo)
+        print(reply)
+        print("LLM chat test end.")
+        print("LLM translation test start.")
+        reply, convo = await Chat("Translate the following into conversational German. Please do not add any prefaces, helpful greetings, or notes. The English phrase is: I am going to the bus station. Could you take care of my cat while I am gone today? \n\n What is the German translation?")
+        print(reply)
+        print("LLM translation test end.")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
